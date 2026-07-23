e786a7f4818c7455851348ca40b294b5e5a6750b
Hyeongjun79 <kwenhj@gmail.com>
2026-07-23 18:11:58 +0900

    feat:vite.config 설정값 변경

--- Project/frontend/vite.config.ts
+++ Project/frontend/vite.config.ts
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: true,
     port: 5174,
-    strictPort: true, 
+    strictPort: true,
   },
 })
