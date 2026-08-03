518b29bdd2b4abb974db2f9dae6a2608937feccd
Hyeongjun79 <kwenhj@gmail.com>
2026-08-03 18:16:35 +0900

    fix

--- Project/backend/src/main/java/com/example/backend/controller/AdminController.java
+++ Project/backend/src/main/java/com/example/backend/controller/AdminController.java
@@ -18,6 +18,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -187,6 +188,7 @@ public class AdminController {
     }
 
     @PostMapping("/admin/posts/{id}/delete")
+    @Transactional
     public String deletePost(@PathVariable("id") Long id) {
         commentRepository.deleteByPostId(id);
         postRepository.deleteById(id);
