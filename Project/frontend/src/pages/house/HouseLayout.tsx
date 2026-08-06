c3a0b99a11f08bce07a9621065febd399265ff99
6y666-sh <9706ysh@gmail.com>
2026-08-06 15:38:34 +0900

    feat: 하우스 레이아웃 스타일 추가 및 카드 배치 개선

--- Project/frontend/src/pages/house/HouseLayout.tsx
+++ Project/frontend/src/pages/house/HouseLayout.tsx
@@ -3,6 +3,7 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { useAuth } from "../../context/AuthContext";
 import HouseSidebar from "./HouseSidebar";
 import "../mypage/MyPageLayout.css";
+import "./HouseLayout.css";
 
 // 마이페이지와 동일한 레이아웃(양식)을 그대로 재사용한다: 왼쪽에 갖다 대면
 // 펼쳐지는 아이콘 사이드바 + 오른쪽 컨텐츠 영역.
@@ -15,7 +16,7 @@ function HouseLayout() {
   }, [token, navigate]);
 
   return (
-    <div className="mypage">
+    <div className="mypage house-mypage">
       <HouseSidebar />
       <main className="mypage-content">
         <Outlet />
